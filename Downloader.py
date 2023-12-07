d577e5d0ccbf1a8fc408b77d50125b3029f333c4
AbrahmCh1 <achavez@duxxsports.com.mx>
2023-12-06 21:11:54 -0600

    Feature (working on it) GUI with html, css and flask

--- Downloader.py
+++ Downloader.py
@@ -1,34 +1,35 @@
 """ Python app developed by Abraham Chavez to download Youtube videos as mp3 or mp4 files, future uptades will include a GUI progress bar and compatibility for more formats"""
 from pytube import YouTube
-import os
+import os, sys, flask, requests
 
-def downloadYoutubeMP4(vid_url, path):
-    yt = YouTube(vid_url)
-    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-    
-    if not os.path.exists(path):
-        os.makedirs(path)
+class Downloader:
+    def downloadYoutubeMP4(vid_url, path):
+        yt = YouTube(vid_url)
+        yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+        
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-    yt.download(path)
+        yt.download(path)
 
-def downloadYoutubeMP3(vid_url, path):
-    yt = YouTube(vid_url)
-    yt = yt.streams.filter(only_audio=True).first()
-    
-    if not os.path.exists(path):
-        os.makedirs(path)
+    def downloadYoutubeMP3(vid_url, path):
+        yt = YouTube(vid_url)
+        yt = yt.streams.filter(only_audio=True).first()
+        
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-    #Add mp3 extension to file
-    path = path + '\\' + yt.title + '.mp3'
-    yt.download(path)
+        #Add mp3 extension to file
+        path = path + '\\' + yt.title + '.mp3'
+        yt.download(path)
 
-url = input('Input url: ')
+    url = input('Input url: ')
 
-# Use raw string (r"") to avoid issues with backslashes
-path = r"C:\Users\abrah\Desktop\Downloader"
-format = input('Input format (mp3/mp4): ')
+    # Use raw string (r"") to avoid issues with backslashes
+    path = r"C:\Users\abrah\Desktop\Downloader"
+    format = input('Input format (mp3/mp4): ')
 
-if format == 'mp3':
-    downloadYoutubeMP3(url, path)
-elif format == 'mp4':
-    downloadYoutubeMP4(url, path)
+    if format == 'mp3':
+        downloadYoutubeMP3(url, path)
+    elif format == 'mp4':
+        downloadYoutubeMP4(url, path)
